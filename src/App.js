61ba4f40224ef9f533abd35be88833303dfb8bcd
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2020-09-05 04:16:59 +0100

    feat(wip): Messing around with axios request and loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
+import ipLocation from './api/ip'
+import SearchBar from './components/SearchBar'
 
 export default class App extends Component {
+  state = { location: {}, isLoading: false }
+  
+  onSearchSubmit = async (term) => {
+    this.setState({isLoading: true});
+
+    const response = await ipLocation.get('', {
+      params: { ipAddress: term }
+    })
+
+    this.setState({isLoading: false});
+    console.log(response);
+    this.setState({ location: response.data.location })
+  }
+
+  renderLoading(){
+    return <div className="test">Loading...</div>;
+  }
+
   render() {
-    return <div>Frontend Mentor Challenge</div>
+    return <div style={{ height: '90vh', widht: '100vw' ,display: 'grid',placeItems: 'center' }}><SearchBar onSubmit={this.onSearchSubmit} />
+    {this.state.isLoading ? this.renderLoading() : ''}
+    <h1>{this.state.location.region}</h1>
+    </div>
   }
 }
